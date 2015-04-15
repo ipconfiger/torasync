3c882ed383294921f43193818426ee8719fd73a6
李明 <superpowerlee@gmail.com>
2015-04-15 20:14:56 +0800

    写一个安装器

---
+++ setup.py
@@ -0,0 +1,17 @@
+#coding=utf8
+__author__ = 'Alexander.Li'
+
+from setuptools import setup
+
+setup(
+    name='torasync',
+    version='0.0.0.1 pre',
+    packages=['torasync'],
+    author='Alexander.Li',
+    author_email='superpowerlee@gmail.com',
+    license='LGPL',
+    install_requires=["tornado>=2.4.1",],
+    description="Run task asynchronously in backend processes in easy way",
+    keywords ='tornado asynchronous multiprocess',
+    url="https://github.com/ipconfiger/torasync"
+)
